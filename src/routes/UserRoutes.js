9861fe3eef289ffe737576e5a07feb836129c8f8
Matheus de Paula Santos <matheus1516paula@gmail.com>
2025-12-29 14:52:08 -0300

    User CRUD

---
+++ src/routes/UserRoutes.js
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+import userController from '../controllers/UserController';
+
+const router = new Router();
+
+router.post('/', userController.store);
+router.get('/', userController.index);
+router.get('/:id', userController.show);
+router.put('/:id', userController.update);
+router.delete('/:id', userController.delete);
+
+export default router;
+
+/*
+ index -> lista todos os usuarios -> get
+ store/create -> cria um novo usuario -> post
+ delete -> apaga um usuario -> delete
+ show -> mostra um usuario -> get
+ update -> atualiza um usuario -> patch ou put
+*/
