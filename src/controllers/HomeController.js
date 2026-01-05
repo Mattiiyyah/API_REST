a68855e2544d3106778b2a51dd488446cdf136ab
Matheus de Paula Santos <matheus1516paula@gmail.com>
2026-01-05 13:33:01 -0300

    Routes aluno

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,25 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    try {
-      const [novoAluno] = await Aluno.findOrCreate({
-        where: { email: 'caio@gmail.com' },
-        defaults: {
-          nome: 'Caio',
-          sobrenome: 'Oliveira',
-          idade: 20,
-          peso: 86,
-          altura: 1.75,
-        },
-      });
-      res.json(novoAluno);
-    } catch (e) {
-      console.log(e);
-      res.status(400).json({
-        errors: e.errors.map((err) => err.message),
-      });
-    }
+    res.json('Index');
   }
 }
 
