42a7e89dd28faa2acd2333da341bc6065328662c
Matheus de Paula Santos <matheus1516paula@gmail.com>
2026-01-05 10:38:06 -0300

    Create Seeds

---
+++ src/database/seeds/20260105131807-criar-usuarios.js
@@ -0,0 +1,38 @@
+const bcrypt = require('bcryptjs');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface) {
+    queryInterface.bulkInsert(
+      'users',
+      [
+        {
+          nome: 'Maria',
+          email: 'maria@gmail.com',
+          password_hash: await bcrypt.hash('123456', 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+
+        {
+          nome: 'Mario',
+          email: 'mario@gmail.com',
+          password_hash: await bcrypt.hash('654321', 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+
+        {
+          nome: 'Marcus',
+          email: 'marcus@gmail.com',
+          password_hash: await bcrypt.hash('987654', 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      ],
+      {},
+    );
+  },
+
+  async down() {},
+};
