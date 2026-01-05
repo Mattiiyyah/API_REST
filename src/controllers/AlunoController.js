57947b79a86bf78eb0eee86823561569217f0caf
Matheus de Paula Santos <matheus1516paula@gmail.com>
2026-01-05 14:42:09 -0300

    CRUD alunos

--- src/controllers/AlunoController.js
+++ src/controllers/AlunoController.js
@@ -1,9 +1,140 @@
 import Aluno from '../models/Aluno';
 
 class AlunoController {
+  //Index
   async index(req, res) {
     const alunos = await Aluno.findAll();
     res.json(alunos);
+  }
+
+  //Store
+  async store(req, res) {
+    try {
+      const aluno = await Aluno.create(req.body);
+
+      const { id, nome, sobrenome, email, idade, peso, altura } = aluno;
+
+      return res.json({
+        id,
+        nome,
+        sobrenome,
+        email,
+        idade,
+        peso,
+        altura,
+      });
+    } catch (e) {
+      console.log(e);
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  //Show
+  async show(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['Faltando o ID'],
+        });
+      }
+
+      const aluno = await Aluno.findByPk(req.params.id);
+
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ['Aluno não existe'],
+        });
+      }
+
+      const { id, nome, sobrenome, email, idade, peso, altura } = aluno;
+
+      return res.json({
+        id,
+        nome,
+        sobrenome,
+        email,
+        idade,
+        peso,
+        altura,
+      });
+    } catch (e) {
+      console.log(e);
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  //Update
+  async update(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['Faltando o ID'],
+        });
+      }
+
+      const aluno = await Aluno.findByPk(req.params.id);
+
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ['Aluno não existe'],
+        });
+      }
+
+      const alunoAtualizado = await aluno.update(req.body);
+
+      const { id, nome, sobrenome, email, idade, peso, altura } =
+        alunoAtualizado;
+
+      return res.json({
+        id,
+        nome,
+        sobrenome,
+        email,
+        idade,
+        peso,
+        altura,
+      });
+    } catch (e) {
+      console.log(e);
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  //Delete
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Faltando o ID'],
+        });
+      }
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ['Aluno não existe'],
+        });
+      }
+
+      await aluno.destroy();
+      return res.json({
+        apagado: true,
+      });
+    } catch (e) {
+      console.log(e);
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
   }
 }
 
